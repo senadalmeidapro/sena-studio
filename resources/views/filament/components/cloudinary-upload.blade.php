549e074a85131f28ea1ba05aa0d0fe2a5360cd37
senadalmeidapro <senadalmeidapro@gmail.com>
2026-09-15 13:32:41 +0100

    fix: make cloudinary alpine component explicit

--- resources/views/filament/components/cloudinary-upload.blade.php
+++ resources/views/filament/components/cloudinary-upload.blade.php
@@ -1,6 +1,6 @@
 <x-dynamic-component :component="$getFieldWrapperView()" :field="$field">
     <div
-        x-data="cloudinaryUploadComponent({!! $getXData() !!})"
+        x-data="window.cloudinaryUploadComponent({!! $getXData() !!})"
         class="fi-fo-cloudinary-upload rounded-xl bg-gray-50 p-4 text-sm ring-1 ring-gray-950/10 dark:bg-white/5 dark:ring-white/10"
     >
         <template x-if="preview">
